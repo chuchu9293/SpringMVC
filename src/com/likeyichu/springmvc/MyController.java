632fd4b9bc783736f965c0b73eee40eb403698cb
yc_du <yc_du@Ctrip.com>
2015-04-27 16:33:15 +0800

    123

--- src/com/likeyichu/springmvc/MyController.java
+++ src/com/likeyichu/springmvc/MyController.java
@@ -4,12 +4,9 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.likeyichu.springmvc.bean.Book;
@@ -21,8 +18,6 @@ public class MyController {
 	
 	@ResponseBody
 	@RequestMapping(value="/setPrice", method = {RequestMethod.GET}, produces="application/json;charset=UTF-8")
-	//没有从对象到序列化内容的处理，这里会出错：
-	//The resource identified by this request is only capable of generating responses with characteristics not acceptable according to the request "accept" headers.
 	public Book fun(HttpServletRequest request, HttpServletResponse response){
 		Double price=Double.valueOf(request.getParameter("price"));
 		book.setPrice(price);
